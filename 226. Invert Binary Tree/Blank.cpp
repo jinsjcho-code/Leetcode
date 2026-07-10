7c1df401594f9c8ec04810f006dd00ee54b339b2
Jin Cho <jin.sj.cho@gmail.com>
2026-07-10 15:50:10 -0700

    Add practice: invert Binary Tree

--- 226. Invert Binary Tree/Blank.cpp
+++ 226. Invert Binary Tree/Blank.cpp
@@ -57,11 +57,6 @@ public:
         // progress
         TreeNode* temp;
         swap(root->left, root->right);
-        /*
-        temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-*/
         invertTree(root->left);
         invertTree(root->right);
 
